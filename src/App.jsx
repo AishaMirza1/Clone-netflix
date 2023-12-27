188498a2c50fbc43b9eceee33968cdae1504ebfd
Home <aishamirza0002@gmail.com>
2023-12-27 11:08:03 +0530

    fix: browser router

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,23 @@
-import { HashRouter, Route, Routes } from "react-router-dom";
-import Home from "./pages/home/Home.jsx";
-import Login from "./pages/login/login.jsx";
+import { createBrowserRouter, RouterProvider, Link } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <div>
+        <h1>Hello World</h1>
+        <Link to="about">About Us</Link>
+      </div>
+    ),
+  },
+  {
+    path: "about",
+    element: <div>About</div>,
+  },
+]);
 
 function App() {
-  return (
-    <>
-      <HashRouter>
-        {/* <Navbar />{" "} */}
-        <Routes>
-          <Route index element={<Home />} />
-          <Route path="stories" element={<Login />} />
-        </Routes>{" "}
-      </HashRouter>
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
